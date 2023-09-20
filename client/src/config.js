344467e55e520f4f073d627058bb0269f292485b
lmakinaa <jaija.ismail@gmail.com>
2023-09-20 20:05:54 +0100

    Stop tracking config.js

--- client/src/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = global.config = {
-    ACCESS_TOKEN: "e1cd598758879ad8003fcaded62551b21dc53288b8cfd0ab58334562a6fd5c3ff7e666328f84abeb1c9cbd73507b62f54ddc09ac273bb8bdb3460b9d7311bff1",
-};
